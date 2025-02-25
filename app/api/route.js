88c616b41f01682bd3944e44d38a890c4eeb4150
MuraruMihai <mihai.mtz007@gmail.com>
2025-02-25 09:34:54 +0200

    Update route.js

--- app/api/route.js
+++ app/api/route.js
@@ -3,5 +3,3 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
     return NextResponse.json({time: new Date().toLocaleString()})
 }
-// 3mrw4fzpFHZmF1DS
-// mongodb+srv://mihaimtz007:3mrw4fzpFHZmF1DS@cluster0.aadow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
